2b3a712e1ba514f831cfed05d09b98af852c06f8
Anthony Warnick (KSU) <ajwarnick@gmail.com>
2019-03-26 17:51:08 -0500

    update script

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@ document.body.addEventListener('click', function(){
     element.classList.toggle("hidden");
 }, true);
 
+
+
+
+var photo1 = document.getElementById("photo1");
+photo1.addEventListener('click', function(){
+    var element = document.getElementById("text1");
+    element.classList.toggle("hide");
+}, true);  
